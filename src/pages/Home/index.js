dcd0a87f715759682f9b15735cee1b49ce7cc11a
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2020-01-17 08:34:19 -0500

    Add more home components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,11 +4,19 @@ import { Grid, Header, Segment } from "semantic-ui-react";
 const Home = () => (
   <div>
     <Grid reversed="mobile" stackable columns={2}>
-      <Grid.Column width={9}>
+      <Grid.Column width={8}>
         <Header as="h2">Journal</Header>
         <Segment>Pellentesque habitant morbi tristique senectus.</Segment>
+        <Segment>Pellentesque habitant morbi tristique senectus.</Segment>
+        <Segment>Pellentesque habitant morbi tristique senectus.</Segment>
+        <Segment>Pellentesque habitant morbi tristique senectus.</Segment>
+        <Segment>Pellentesque habitant morbi tristique senectus.</Segment>
       </Grid.Column>
-      <Grid.Column width={7}>
+      <Grid.Column width={8}>
+        <Header as="h2">Alerts</Header>
+        <Segment>Pellentesque habitant morbi tristique senectus.</Segment>
+        <Header as="h2">Stats</Header>
+        <Segment>Pellentesque habitant morbi tristique senectus.</Segment>
         <Header as="h2">Calendar</Header>
         <Segment>Pellentesque habitant morbi tristique senectus.</Segment>
       </Grid.Column>
