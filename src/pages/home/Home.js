3b4f0c3ce1aef6b4148f1d4a82dc74b8b1e8d3b3
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2020-01-20 20:56:47 -0500

    Add stats section to home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import Stats from "./stats/Stats";
 import Notifications from "./notifications/Notifications";
 import Techniques from "./techniques/Techniques";
 import Journal from "./journal/Journal";
 
 const Home = () => (
   <div>
+    <Stats />
     <Notifications />
     <Techniques />
     <Journal />
---
+++ src/pages/home/stats/Stats.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Icon } from "semantic-ui-react";
+
+const Stats = () => (
+  <>
+    <center>
+      <Icon fitted name="line chart" size="massive" />
+    </center>
+  </>
+);
+
+export default Stats;
